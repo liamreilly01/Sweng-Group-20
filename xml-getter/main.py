8b5c3f7b3a223b0fa1d6a4452bf44f7067951540
Jake McKenna <jamckenn@tcd.ie>
2023-02-03 15:33:19 +0000

    Added code to convert the metadata xml tag to json and print the object to the console

--- xml-getter/main.py
+++ xml-getter/main.py
@@ -1,6 +1,8 @@
 
 import requests
-
+import xml.etree.ElementTree as ET
+import xmltodict
+import json
 
 
 acts_2022 = []
@@ -8,6 +10,13 @@ acts_2022 = []
 for act_no in range(1, 53): #1 - 52 acts from 2022
     act_url = "https://www.irishstatutebook.ie/eli/2022/act/" + str(act_no) + "/enacted/en/xml"
     act_response = requests.get(act_url)
+
+    # Acts 2 and 4 have badly formed xml and cause errors
+    if act_no != 2 and act_no != 4:
+        act = ET.fromstring(act_response.content)
+        metadata = xmltodict.parse(ET.tostring(act.find('metadata')))
+        s = json.dumps(metadata)
+        print(s)
 
     if act_response.status_code == 200: #200 -> file found
         acts_2022.append(act_response.text)
