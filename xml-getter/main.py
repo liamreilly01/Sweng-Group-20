59d5803b3f33fed5be715e209ea92d737c70f2f6
Declan <mccabed7@tcd.ie>
2023-02-07 16:02:34 +0000

    Cleaned up code.  Array acts_2022 stores act now.

--- xml-getter/main.py
+++ xml-getter/main.py
@@ -12,15 +12,13 @@ for act_no in range(1, 53): #1 - 52 acts from 2022
     act_response = requests.get(act_url)
 
     # Acts 2 and 4 have badly formed xml and cause errors
-    if act_no != 2 and act_no != 4:
+    if act_no != 2 and act_no != 4 and act_response.status_code == 200:
         act = ET.fromstring(act_response.content)
+        acts_2022.append(act)
+        print("Found and added act: " + str(act_no))
         metadata = xmltodict.parse(ET.tostring(act.find('metadata')))
         s = json.dumps(metadata)
         print(s)
-
-    if act_response.status_code == 200: #200 -> file found
-        acts_2022.append(act_response.text)
-        print("Found and added act: " + str(act_no))
     else: #bad status code, couldn't retrieve file for some reason
         print("Couldn't find act:" + str(act_no) + "   Status code: " + str(act_response.status_code))
 
