8aa30b96211ffd6539aecc875f60e6e80a430189
Jake McKenna <jamckenn@tcd.ie>
2023-02-17 11:48:08 +0000

    Swapped to xml scraper, added fada tag replacer function and made it loop through the acts of a given year

--- xml-getter/soup.py
+++ xml-getter/soup.py
@@ -4,16 +4,68 @@ from lxml import etree
 from main import acts_to_json_format
 from bs4 import BeautifulSoup
 
+def replace_accents(tag):
+    try:
+        tag.afada.replace_with('á')
+    except:
+        pass
+    try:
+        tag.Afada.replace_with("Á")
+    except:
+        pass
+
+    try:
+        tag.efada.replace_with('é')
+    except:
+        pass
+    try:
+        tag.Efada.replace_with("É")
+    except:
+        pass
+
+    try:
+        tag.ifada.replace_with('í')
+    except:
+        pass
+    try:
+        tag.Ifada.replace_with("Í")
+    except:
+        pass
+
+    try:
+        tag.ofada.replace_with('ó')
+    except:
+        pass
+    try:
+        tag.Ofada.replace_with("Ó")
+    except:
+        pass
+
+    try:
+        tag.ufada.replace_with("ú")
+    except:
+        pass
+    try:
+        tag.Ufada.replace_with("Ú")
+    except:
+        pass
 
 def fetch_acts(year):
     file = open("acts.json", "w", encoding="utf-8")  # "w" = overwrite, "a" = append
 
-    act_url = "https://www.irishstatutebook.ie/eli/" + str(year) + "/act/10/enacted/en/html"
+    act_url = "https://www.irishstatutebook.ie/eli/" + str(year) + "/act/1/enacted/en/xml"
     act_response = requests.get(act_url)
     act_no = 1
-    soup = BeautifulSoup(act_response.content, "html.parser")
-    title = soup.title.text
-    print(title)
+
+    while act_response.status_code == 200:
+        soup = BeautifulSoup(act_response.content, "xml")
+        title = soup.metadata.title
+        replace_accents(title) # 2022 Act 33 does not use fada tags. e.g. $afada instead of <afada>
+        print(title.text)
+
+        act_no += 1
+        act_url = "https://www.irishstatutebook.ie/eli/" + str(year) + "/act/" + str(act_no) + "/enacted/en/xml"
+        act_response = requests.get(act_url)
 
 
 fetch_acts(2022)
