bbb8bc1f53c744dbf65236ea37294f93ee7cfcbb
Mahirm01 <masudm@tcd.ie>
2023-03-21 15:48:09 +0000

    Added Chatbot Frontend

---
+++ djangoSweng/blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'blog/index.html' )
+
+def specific(request):
+        list1 = [1,2,3,4]
+        return HttpResponse(list1)
+
+
+    
