3ea5e3db2f44abd914142f7753cdfcf713b76c01
Declan <mccabed7@tcd.ie>
2023-03-21 11:38:03 +0000

    Added test act and migrated legislation

--- Chatbot/database/models.py
+++ Chatbot/database/models.py
@@ -6,6 +6,8 @@ class Legislation(models.Model):
     title = models.CharField(max_length=255)
     url = models.URLField()
     description = models.TextField()
+    def __str__(this):
+        return this.title
 
 
 class Questionanswer(models.Model):
