6c84cfd6b5f8dc6177d8aa60a043673645883854
Grace Bokunde <123752918+GraceBokunde@users.noreply.github.com>
2023-03-26 14:47:39 +0100

    Update urls.py

--- Chatbot/database/urls.py
+++ Chatbot/database/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('database/', views.database, name='database'),
+    path('', views.database, name='database'),
     path('legislationList/', views.legislationList, name="legislationList"),
     path('legislationList/<str:key>', views.legislationDetail, name="legislation")
 ]
