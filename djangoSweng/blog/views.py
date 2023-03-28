c8d5b7790e285798a932f8f562c5b242325eaea5
liamreilly01 <98739910+liamreilly01@users.noreply.github.com>
2023-03-28 13:52:25 +0100

    Update views.py

--- djangoSweng/blog/views.py
+++ djangoSweng/blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .apps.py import getChatbotOutput
 # Create your views here.
 
-def index(request):
-    return render(request, 'blog/index.html' )
+def chatbot_view(request):
+    if request.method == 'POST':
+        user_input = request.POST.get('user_input', '')
+        # process user input and generate chatbot response
+        return HttpResponse(getChatbotOutput)
+    else:
+        # render the initial form
+        return render(request, 'index.html')
 
 def specific(request):
         list1 = [1,2,3,4]
