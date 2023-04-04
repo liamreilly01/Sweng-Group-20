66aa3e50fb28777be8a83833b7104b3624aca3f8
Jake McKenna <jamckenn@tcd.ie>
2023-04-04 22:56:31 +0100

    Responses are now generated from the database

--- Chatbot/database/json_example.py
+++ Chatbot/database/json_example.py
@@ -1,7 +1,17 @@
 #A short script demoing how to obtain data from the website in a json format
 #Note: server must be running locally on your computer for this to work
 import requests;    #needed to for requests function
+import json
 
 json_response = requests.get("http://127.0.0.1:8000/legislationList/") #html 'get' function
-print(json_response.status_code) #if 200, everything went well
-print(json_response.content)     #prints file
+actsString = str(json_response.content)
+actsString = actsString.replace("b'", "{\"data\":", 1).replace("}]'", "}]}", 1)
+actsString = actsString.encode("unicode_escape")
+#out_file = open("myfile.json", "w")
+  
+#json.dump(actsString, out_file)
+acts = json.loads(actsString)
+print(acts["data"][1])
+#print(actsString)
+#print(json_response.status_code) #if 200, everything went well
+#print(json_response.content)     #prints file
--- Chatbot/database/main.py
+++ Chatbot/database/main.py
@@ -1,6 +1,7 @@
 # USAGE:
 # pip install transformers
 import os
+import requests
 from Chatbot.settings import BASE_DIR
 
 
@@ -9,18 +10,25 @@ def getChatbotOutput(question):
     import json
     from transformers import pipeline
     from pathlib import Path
-    try:
-        sampleActs = open('../acts.json', "r", encoding="utf-8")
-        print("opended");
-        try:
-            sampleActsDictionary = json.loads(sampleActs.read())
-            print("loaded");
-        except:
-            print("ERROR loading and reading Sample_Acts.json")
-        finally:
-            sampleActs.close()
-    except:
-        print("ERROR opening Sample_Acts.json")
+
+    json_response = requests.get("http://127.0.0.1:8000/legislationList")
+    actsString = str(json_response.content)
+    actsString = actsString.replace("b'", "{\"acts\":", 1).replace("}]'", "}]}", 1)
+    actsString = actsString.encode("unicode_escape")
+    #print(actsString)
+    acts = json.loads(actsString)
+    # try:
+    #     sampleActs = open('../acts.json', "r", encoding="utf-8")
+    #     print("opended");
+    #     try:
+    #         sampleActsDictionary = json.loads(sampleActs.read())
+    #         print("loaded");
+    #     except:
+    #         print("ERROR loading and reading Sample_Acts.json")
+    #     finally:
+    #         sampleActs.close()
+    # except:
+    #     print("ERROR opening Sample_Acts.json")
 
     # initialise the Question-Answer Pipeline
     pipeline = pipeline("question-answering", model=(os.path.join(BASE_DIR, "model")))
@@ -32,8 +40,8 @@ def getChatbotOutput(question):
     # iterate through each act's description
     # !!! Very inefficient !!!
 
-    for act in sampleActsDictionary["2022"]["acts"]:
-        context = act["details"] # text that model will read
+    for i in range(1,4):
+        context = acts["acts"][i]["details"] # text that model will read
         result = pipeline(question=question, context=context) # generate response
         if result["score"] > maxScore:
             maxScore = result["score"]
